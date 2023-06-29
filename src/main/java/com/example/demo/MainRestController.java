d7889f10f63876eef2bd944ff25ca2dbcb856e53
tiwari-2612 <uttu3101@gmail.com>
2023-06-29 10:41:19 +0530

    28/06/2023_Commit

--- src/main/java/com/example/demo/MainRestController.java
+++ src/main/java/com/example/demo/MainRestController.java
@@ -5,6 +5,7 @@ import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +18,16 @@ public class MainRestController {
     @Autowired
     UserdetailRepository userdetailRepository;
 
+    @GetMapping("dummy")
+    public Userdetail dummydetail() {
+        Userdetail userdetail = new Userdetail();
+        userdetail.setUsername("tiwarri@2612");
+        userdetail.setEmail("uttu3101");
+        userdetail.setFname("Utkarsh");
+        userdetail.setLname("Tiwari");
+        userdetail.setPhone("9910002560 ");
+        return userdetail;
+    }
 
     @PostMapping("getuserbyemail")
     public ResponseEntity<Userdetail> getUserdetailsByEmail(@RequestParam("email") String email){
